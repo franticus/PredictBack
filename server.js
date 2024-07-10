fd446f7cd468133ade1c84634d1fed9746d0778e
S.Zelensky <zelenstein@gmail.com>
2024-07-10 17:13:27 +0300

    git

--- server.js
+++ server.js
@@ -54,7 +54,6 @@ app.post('/data', cors(corsOptions), (req, res) => {
     const ip = req.clientIp;
     const geo = geoip.lookup(ip);
     const country = geo ? geo.country : 'Unknown';
-    console.log('country:', country);
 
     const newEntry = {
       name: req.body.name,
